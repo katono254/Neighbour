fb1ef0c483899257d7511f1dbd803c854b13844f
Greg <blackrocket.47@gmail.com>
2020-02-26 15:46:36 +0300

    Added media in the app

--- Neighbour/settings.py
+++ Neighbour/settings.py
@@ -92,7 +92,8 @@ DATABASES = {
         'PASSWORD':'greg1234',
     }
 }
-
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
